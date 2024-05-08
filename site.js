ebd266b9b318caedd7d909a9e7d9ed340c5fa5a2
TMcClure123 <jumpscares4days@gmail.com>
2024-05-08 14:19:39 -0400

    steps 1-5

--- site.js
+++ site.js
@@ -33,6 +33,10 @@ const vue_app = Vue.createApp({
         return {
             // This holds your movies.json data.
             movies: [],
+            title: 'IMDB' + ' Tylers Top 8 Movies',
+            owner: 'Tyler',
+            github:'https://github.com/TMcClure123?tab=repositories',
+            
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
          
       }
