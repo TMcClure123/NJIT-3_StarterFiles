75704db01198f32aaadf29613a66543077788a9b
TMcClure123 <jumpscares4days@gmail.com>
2024-05-20 11:44:44 -0400

    Finished

--- site.js
+++ site.js
@@ -24,70 +24,65 @@
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            title: 'IMDB' + ' Tylers Top 8 Movies',
-            owner: 'Tyler',
-            github:'https://github.com/TMcClure123?tab=repositories',
-            
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+            return {
+                  // This holds your movies.json data.
+                  movies: [],
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+                  title: "IMDB + Tyler's Top 8 Movies",
+                  owner: 'Tyler McClure',
+                  github: 'https://github.com/TMcClure123/Portfolio'
+            }
+      },
       methods: {
-        getMonthText(dateArray){
-
-            let month
-
-        switch (dateArray[1]) {
-            case 1: 
-            month = "January";
-                  break
-            case 2: 
-            month = "February";
-                  break
-            case 3: 
-            month = "March";
-                  break
-            case 4: 
-            month = "April";
-                  break
-            case 5: 
-            month = "May";
-                  break
-            case 6: 
-            month = "June";
-                  break
-            case 7: 
-            month = "July";
-                  break
-            case 8: 
-            month = "August";
-                  break
-            case 9: 
-            month = "September";
-                  break
-            case 10: 
-            month = "October";
-                  break
-            case 11: 
-            month = "November";
-                  break
-            case 12: 
-            month = "December";
-                  break
-        }
-            return month + ' ' + dateArray[2]+ ', ' + dateArray[0]
-
-        },
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray) {
+                  let month = ""
+                  switch (dateArray[1]) {
+
+                        case 1:
+                              month = "January"
+                              break
+                        case 2:
+                              month = "February"
+                              break
+                        case 3:
+                              month = "March"
+                              break
+                        case 4:
+                              month = "April"
+                              break
+                        case 5:
+                              month = "May"
+                              break
+                        case 6:
+                              month = "June"
+                              break
+                        case 7:
+                              month = "July"
+                              break
+                        case 8:
+                              month = "August"
+                              break
+                        case 9:
+                              month = "September"
+                              break
+                        case 10:
+                              month = "October"
+                              break
+                        case 11:
+                              month = "November"
+                              break
+                        case 12:
+                              month = "December"
+                  }
+                  return month + " " + dateArray[2] + ", " + dateArray[0]
+            },
             posterClick(index) {
                  
                   if (this.movies[index].posterindex > this.movies[index].posters.length - 2) {
@@ -97,6 +92,12 @@ const vue_app = Vue.createApp({
                   }
 
             },
+            timeText(min) {
+                  let hours = Math.floor(min / 60)
+                  let minutes = min % 60
+                  return hours + 'h ' + minutes + 'm '
+            }
+
       }
 })
 
